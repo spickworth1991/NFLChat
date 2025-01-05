d971e4e97e04dbeb6fdbdfbb0b6f37205ddc5be2
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2025-01-05 01:38:27 -0500

    Testing

--- public/script.js
+++ public/script.js
@@ -22,8 +22,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const searchingMessage = addMessage('Searching for an answer...', 'bot');
 
-    //fetch('https://nfl-chat-bot.onrender.com/chat', {
-    fetch('http://127.0.0.1:5000/chat', {
+    fetch('https://nfl-chat-bot.onrender.com/chat', {
+    //fetch('http://127.0.0.1:5000/chat', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ message: userMessage }),
